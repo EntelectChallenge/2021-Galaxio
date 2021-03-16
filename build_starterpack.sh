c19f21ed7cb8310057dc86627e75e310f33a2693
Fabio Loreggian <fabio.loreggian@entelect.co.za>
2021-03-16 15:00:20 +0200

    Update build file agaain

--- build_starterpack.sh
+++ build_starterpack.sh
@@ -6,5 +6,5 @@ cp -r ./engine-publish ./starter-pack/
 cp -r ./runner-publish ./starter-pack/
 cp -r ./logger-publish ./starter-pack/
 cp -r ./ec-compose ./starter-pack/
-cp run.sh ./starter-pack/
+cp ./run.sh ./starter-pack/
 cp -r ./starter-bots ./starter-pack/
