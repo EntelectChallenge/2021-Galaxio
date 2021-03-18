ec1e153b25e09298a6eb0b978694697fa9b1a842
Fabio Loreggian <fabio.loreggian@entelect.co.za>
2021-03-18 11:48:19 +0200

    Update run file

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ./runner-publish/ && dotnet GameRunner.dll &
-cd ./engine-publish/ && dotnet Engine.dll &
-cd ./logger-publish/ && dotnet Logger.dll &
-cd ./reference-bot-publish/ && dotnet ReferenceBot.dll &
-cd ./reference-bot-publish/ && dotnet ReferenceBot.dll &
-cd ./reference-bot-publish/ && dotnet ReferenceBot.dll &
+cd ./engine-publish/ && sleep 1 && dotnet Engine.dll &
+cd ./logger-publish/ && sleep 1 && dotnet Logger.dll &
+cd ./reference-bot-publish/ && sleep 3 && dotnet ReferenceBot.dll &
+cd ./reference-bot-publish/ && sleep 3 && dotnet ReferenceBot.dll &
+cd ./reference-bot-publish/ && sleep 3 && dotnet ReferenceBot.dll &
 
 $SHELL
