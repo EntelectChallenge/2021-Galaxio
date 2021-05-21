a59f0ab96eab8b8ecbda490bafc4823d88f9c0af
Juan de Bruin <juan@debruin.dev>
2021-05-21 22:56:03 +0200

    Add new object types

--- starter-bots/KotlinBot/src/main/kotlin/galaxio/model/enums/ObjectTypes.kt
+++ starter-bots/KotlinBot/src/main/kotlin/galaxio/model/enums/ObjectTypes.kt
@@ -5,7 +5,9 @@ enum class ObjectTypes(val value: Int) {
 	FOOD(2),
 	WORMHOLE(3),
 	GAS_CLOUD(4),
-	ASTEROID_FIELD(5);
+	ASTEROID_FIELD(5),
+	TORPEDO_SALVO(6),
+	SUPERFOOD(7);
 
 	companion object {
 		fun valueOf(value: Int): ObjectTypes = values().first { it.value == value }
